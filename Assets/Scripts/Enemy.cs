e1cd397310e85d62f3adc0e60e507cbac1665560
unknown <rodrigolinhares@gmail.com>
2023-04-27 11:29:46 -0300

    Enemy now dies and respawns.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Player player;
-    private float moveSpeed = 5f;
+    private int health = 3;
+    private float moveSpeed = 2f;
     private Rigidbody2D body, playerBody;
 
     // Start is called before the first frame update
@@ -30,5 +31,24 @@ public class Enemy : MonoBehaviour
         if(Mathf.Abs(playerBody.position.y - body.position.y) > 0.1)
             yDirection = System.Math.Sign(playerBody.position.y - body.position.y);
         body.velocity = new Vector2(xDirection * moveSpeed, yDirection * moveSpeed);
+        if (health < 1)
+        {
+            health = 3;
+            Teleport();
+        }
+
+    }
+
+    private void Teleport()
+    {
+        float xPosition = Random.Range(-9f, 9f);
+        float yPosition = Random.Range(-3.8f, 3.8f);
+        gameObject.transform.position = new Vector3(xPosition, yPosition);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Projectile")
+            health--;
     }
 }
